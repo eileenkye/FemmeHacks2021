18d4fb2243b5be30fe3f685215032e5060590617
eileeny <eileen.k.ye@gmail.com>
2021-02-06 15:17:52 -0500

    Edited the profile page

---
+++ Profile.js
@@ -0,0 +1,20 @@
+function setGoal() {
+	var goal = prompt("How many hours do you hope to complete?");
+	var message = "Goal: " + goal + " hours";
+	document.getElementById("goal").innerHTML = message;
+
+}
+
+function addHours() {
+	var newHours = prompt("Congratulations! How many hours did you complete?");
+	var hours = document.getElementById("completed").innerHTML;
+	hours = hours.slice(17, hours.length-6);
+	var hoursnum = parseFloat(hours);
+	hoursnum += parseFloat(newHours);
+	var message = "Hours Completed: " + hoursnum + " hours";
+	document.getElementById("completed").innerHTML = message;
+}
+
+function resetHours() {
+	document.getElementById("completed").innerHTML = "Hours Completed: 0 hours";
+}
